7cc9e8cff64415371f1358af84a43e0bad596ab3
Kristopher Gray <kristopher.gray99@gmail.com>
2023-04-23 17:26:33 +0200

    after report screen

--- one-pager/src/main.ts
+++ one-pager/src/main.ts
@@ -3,3 +3,12 @@ import './main.scss'
 
 // Import all of Bootstrap's JS
 import 'bootstrap/dist/js/bootstrap.js'
+
+const homePageEl = document.querySelector('#home-page') as HTMLDivElement
+const reportEl = document.querySelector('#report') as HTMLDivElement
+const reportTreeBtnEl = document.querySelector('#report-tree-btn') as HTMLDivElement
+
+reportTreeBtnEl.addEventListener('click', () => {
+	homePageEl.classList.add('hide')
+	reportEl.classList.remove('hide')
+})
