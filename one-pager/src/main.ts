8844bfe3695a64f9c45747632229b58a3e5b9bde
Kristopher Gray <kristopher.gray99@gmail.com>
2023-04-23 21:21:16 +0200

    kinda done

--- one-pager/src/main.ts
+++ one-pager/src/main.ts
@@ -4,11 +4,33 @@ import './main.scss'
 // Import all of Bootstrap's JS
 import 'bootstrap/dist/js/bootstrap.js'
 
+// div
 const homePageEl = document.querySelector('#home-page') as HTMLDivElement
-const reportEl = document.querySelector('#report') as HTMLDivElement
-const reportTreeBtnEl = document.querySelector('#report-tree-btn') as HTMLDivElement
+const reportContainerEl = document.querySelector('#report-container') as HTMLDivElement
+const afterReportContainerEl = document.querySelector('#after-report') as HTMLDivElement
 
+// form
+const reportTreeBtnEl = document.querySelector('#report-tree-btn') as HTMLButtonElement
+const sendFormBtnEl = document.querySelector('#send-form-btn') as HTMLButtonElement
+const homePageBtnEl = document.querySelector('#home-page-btn') as HTMLButtonElement
+
+// repoprt tree
 reportTreeBtnEl.addEventListener('click', () => {
 	homePageEl.classList.add('hide')
-	reportEl.classList.remove('hide')
+	reportContainerEl.classList.remove('hide')
+	window.scrollTo(0, 0);
+})
+
+// send report
+sendFormBtnEl.addEventListener('click', () => {
+	reportContainerEl.classList.add('hide')
+	afterReportContainerEl.classList.remove('hide')
+	window.scrollTo(0, 0);
+})
+
+// homepage
+homePageBtnEl.addEventListener('click', () => {
+	afterReportContainerEl.classList.add('hide')
+	homePageEl.classList.remove('hide')
+	window.scrollTo(0, 0);
 })
